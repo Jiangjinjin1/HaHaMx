763749bc2cde15d69f3a2c649250d4ce6f3389da
landandan <383326809@qq.com>
2018-07-03 13:38:21 +0800

    添加常量文件

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,20 @@
 /**
  * @flow
  */
+import React from 'react'
+import {
+  Platform,
+  StatusBar,
+  Dimensions
+} from 'react-native'
+
+const isLessKitKat = Platform.OS === 'android' && Platform.Version < 19
+
+const androidTop = isLessKitKat ? 0 : StatusBar.currentHeight
+
+const deviceHeight = Dimensions.get('window').height
+
+export const Screen = Platform.OS === 'ios' ? Dimensions.get('window') : {
+  ...Dimensions.get('window'),
+  height: deviceHeight - androidTop
+}
