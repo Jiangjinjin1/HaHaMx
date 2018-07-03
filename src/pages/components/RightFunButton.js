b76556bef721e154062f59427e327ace2b134213
landandan <383326809@qq.com>
2018-07-03 13:34:07 +0800

    优化右边栏按钮

--- src/pages/components/RightFunButton.js
+++ src/pages/components/RightFunButton.js
@@ -4,32 +4,62 @@
 import React from 'react'
 import {
   View,
-  TouchableOpacity,
+  Alert,
   DeviceEventEmitter,
 } from 'react-native'
-import Icon from "./Icon";
+import { Icon } from 'react-native-elements'
+import { Screen } from '../../utils/constant'
 
 const RightFunButton = (props) => {
-  const { onPress = () => {}} = props
+  const { closeImageView = () => {}} = props
+  const { width, height } = Screen
+
   return (
     <View
       style={{
         position: 'absolute',
-        right: 20,
-        bottom: 50,
+        right: width / 25,
+        bottom: height / 12,
+        opacity: 0.7,
       }}
     >
-      <TouchableOpacity
-        onPress={() => DeviceEventEmitter.emit('toggleComment', {})}
-      >
-        <Icon
-          suite={'FontAwesome'}
-          name={'commenting'}
-          size={40}
-          color={'#F0F0F0'}
-          style={{ marginRight: 5 }}
-        />
-      </TouchableOpacity>
+      <Icon
+        raised
+        type={'font-awesome'}
+        name={'download'}
+        size={25}
+        color={'#ffdc00'}
+        onPress={() => {
+          Alert.alert(
+            '提示',
+            '开发中',
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: false }
+          )
+        }}
+      />
+
+      <Icon
+        raised
+        type={'font-awesome'}
+        name={'commenting'}
+        size={25}
+        color={'#ffdc00'}
+        onPress={() => DeviceEventEmitter.emit('toggleComment', {
+          needReset: false,
+        })}
+      />
+
+      <Icon
+        raised
+        name='arrow-left'
+        type='entypo'
+        color='#ffdc00'
+        size={25}
+        onPress={() => closeImageView()}
+      />
     </View>
   )
 }
